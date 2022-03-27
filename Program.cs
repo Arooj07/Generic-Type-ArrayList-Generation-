9ca8ffeab08f5469046c490493a5b47e2c02ae6e
Arooj07 <100277795+Arooj07@users.noreply.github.com>
2022-03-27 13:02:54 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace AroojQuiz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = new List<int>();
+            arr.Add(3);
+            arr.Add(4);
+            arr.Add(5);
+            arr.Add(1);
+            arr.Add(8);
+            for (int i = 0; i < arr.Count; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+            arr.Reverse();
+            Console.WriteLine();
+            foreach (int i in arr)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("-------------------------");
+            arr.Sort();
+
+            foreach (int i in arr)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
+           
